095099228e16e8336dc0580ae495d2941b4403de
Jeff Austin <jeffaustin81@Jeffs-MacBook-Pro.local>
2015-06-26 19:29:53 -0700

    temp app

---
+++ branching/temperature.php
@@ -0,0 +1,35 @@
+<?php
+    $temp = $_GET ["temp"];
+    $favorite_number = rand(1, 20);
+    $message = tempResponse($temp);
+
+    function tempResponse($temp)
+    {
+
+        if ($temp < 60) {
+            return "It's cold out!";
+        }
+        elseif ($temp > 80) {
+            return "It's too hot!";
+        }
+        else {
+            return "It's lovely out!";
+        }
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <title>How hot is it?</title>
+</head>
+<body>
+
+    <div class="container">
+
+        <h1><?php echo $message;?></h1>
+
+</body>
+</html>
