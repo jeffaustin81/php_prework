83854e9d769abd85f1d244b5f748d1febd06062e
Jeff Austin <jeffaustin81@gmail.com>
2015-06-26 20:00:02 -0700

    unfinished

--- variables/band.php
+++ variables/band.php
@@ -18,15 +18,6 @@
 </head>
 <body>
     <h1>Welcome! We are fans of <?php echo $band_name; ?>!</h1>
-    <p>Dear <?php echo $band_member2; ?>,</p>
-    <p>How are you? I hope that you are having a nice weekend. I'm vacationing in the mountains of Tibet while I learn programming! </p>
-    <p><?php echo $band_member5; ?>, you would not believe how cold it is here. I should have gone to Hawaii instead. But I like programming a lot, so I've got that going for me. </p>
-    <p>How is <?php echo $band_member6; ?>? Say hi for me!</p>
-    <p>Give <?php echo $band_member4; ?> a belly rub for me!</p>
-    <p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>
-    <p>Cheers,</p>
-    <p><?php echo $band_member3; ?></p>
-    <p><?php echo $band_member1; ?></p>
-    <p><?php echo $band_genre; ?></p>
+<!-- Application unfinished. Still in progress. -->
 </body>
 </html>
