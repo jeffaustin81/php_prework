22b9a8bebb6c4ce56cc0193bac4b9b3201c94e00
Jeff Austin <jeffaustin81@gmail.com>
2015-06-27 13:53:59 -0700

    fixed equal operator

--- branching/fortune.php
+++ branching/fortune.php
@@ -1,40 +1,40 @@
 <?php
     function yourFortune($month)
     {
-        if (strtoupper($month) = "JANUARY") {
+        if (strtoupper($month) == "JANUARY") {
             return "All of your New Years resolutions will come true!";
         }
-        elseif (strtoupper($month) = "FEBRUARY") {
+        elseif (strtoupper($month) == "FEBRUARY") {
             return "You will fall in love with a special someone!";
         }
-        elseif (strtoupper($month) = "MARCH") {
+        elseif (strtoupper($month) == "MARCH") {
             return "Luck will shine upon you!";
         }
-        elseif (strtoupper($month) = "APRIL") {
+        elseif (strtoupper($month) == "APRIL") {
             return "A golden egg of opportunity will fall into your lap.";
         }
-        elseif (strtoupper($month) = "MAY") {
+        elseif (strtoupper($month) == "MAY") {
             return "Never fear! The end of something marks the start of something new.";
         }
-        elseif (strtoupper($month) = "JUNE") {
+        elseif (strtoupper($month) == "JUNE") {
             return "The sun will shine on you if you let it.";
         }
-        elseif (strtoupper($month) = "JULY") {
+        elseif (strtoupper($month) == "JULY") {
             return "You can’t steal second base and keep your foot on first.";
         }
-        elseif (strtoupper($month) = "AUGUST") {
+        elseif (strtoupper($month) == "AUGUST") {
             return "You will be sharing great news with all the people you love.";
         }
-        elseif (strtoupper($month) = "SEPTEMBER") {
+        elseif (strtoupper($month) == "SEPTEMBER") {
             return "You have the power to write your own fortune.";
         }
-        elseif (strtoupper($month) = "OCTOBER") {
+        elseif (strtoupper($month) == "OCTOBER") {
             return "You will be successful in your work.";
         }
-        elseif (strtoupper($month) = "NOVEMBER") {
+        elseif (strtoupper($month) == "NOVEMBER") {
             return "You will enjoy good health; you will be surrounded by luxury.";
         }
-        elseif (strtoupper($month) = "DECEMBER") {
+        elseif (strtoupper($month) == "DECEMBER") {
             return "You will make change for the better.";
         }
     }
